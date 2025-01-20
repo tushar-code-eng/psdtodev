98c95b3cbf1a6327a4dd9fd9570cc4eaeae57c47
Tushar <tusharbajaj10@gmail.com>
2025-01-21 02:10:44 +0530

    changes

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -1,5 +1,4 @@
 import { cn } from "../../lib/utils";
-import { useNavigate } from "react-router-dom";
 
 export const BentoGrid = ({
   className,
@@ -21,6 +20,7 @@ export const BentoGrid = ({
 };
 
 export const BentoGridItem = ({
+  color,
   url,
   className,
   title,
@@ -28,6 +28,7 @@ export const BentoGridItem = ({
   header,
   icon,
 }: {
+  color?:string,
   url?: string;
   className?: string;
   title?: string | React.ReactNode;
@@ -35,20 +36,23 @@ export const BentoGridItem = ({
   header?: React.ReactNode;
   icon?: React.ReactNode;
 }) => {
-  const navigate = useNavigate();
-
   return (
     <div
-      onClick={() => url && navigate(url)}
+      onClick={(e) => {
+        e.preventDefault();
+        if (url) {
+          window.open(url, "_blank");
+        }
+      }}
       className={cn(
-        " cursor-pointer row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border justify-between flex flex-col space-y-4",
+        `cursor-pointer row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-700 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-${color} bg-neutral-900 shadow-sm shadow-neutral-300 justify-between flex flex-col space-y-4`,
         className
       )}
     >
       {header}
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         {icon}
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+        <div className="font-sans font-bold text-neutral-200 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
         <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,91 +1,134 @@
-import img1 from '/outer/img1.png'
-import img2 from '/outer/img2.png'
+import img1 from "/outer/img1.png";
+import img2 from "/outer/img2.png";
+import img3 from "/outer/img3.png";
+import img4 from "/outer/img4.png";
+
+import upwork from "/outer/upwork.svg";
+import github from "/outer/github.svg";
+import twitter from "/outer/twitter.svg";
 
 import { BentoGrid, BentoGridItem } from "../components/ui/bento-grid";
 import {
   IconArrowWaveRightUp,
-  IconBoxAlignRightFilled,
-  IconBoxAlignTopLeft,
+  // IconBoxAlignRightFilled,
+  // IconBoxAlignTopLeft,
   IconClipboardCopy,
   IconFileBroken,
   IconSignature,
-  IconTableColumn,
+  // IconTableColumn,
 } from "@tabler/icons-react";
 
 const Home = () => {
   return (
-    <div>
-        <div className="text-center text-4xl my-4 font-bold">
-            Here are some Web pages built by me.
+    <div className="w-full bg-[#121212]">
+      <div className="bg-neutral-900 py-2 px-4 flex items-center justify-between  text-neutral-300">
+        <div className="font-bold">Tushar Bajaj</div>
+        <div className="flex items-center justify-center gap-5">
+          <div
+            className="w-6 cursor-pointer"
+            onClick={() =>
+              window.open(
+                "https://www.upwork.com/freelancers/~01adae8673d8d4410e"
+              )
+            }
+          >
+            <img src={upwork} alt="" />
+          </div>
+          <div
+            className="w-6 cursor-pointer"
+            onClick={() => window.open("https://github.com/tushar-code-eng")}
+          >
+            <img src={github} alt="" />
+          </div>
+          <div
+            className="w-6 cursor-pointer"
+            onClick={() => window.open("https://x.com/BajajTushar05")}
+          >
+            <img src={twitter} alt="" />
+          </div>
         </div>
-      <BentoGrid className="max-w-4xl mx-auto ">
-        {items.map((item, i) => (
-          <BentoGridItem
-            key={i}
-            title={item.title}
-            description={item.description}
-            header={item.header}
-            icon={item.icon}
-            className={i === 3 || i === 6 ? "md:col-span-2" : ""}
-            url={item.url}
-          />
-        ))}
-      </BentoGrid>
+      </div>
+      <div className=" max-w-6xl mx-auto mt-4">
+        <div className="text-center text-neutral-300 text-6xl pt-6 pb-10 font-extrabold">
+          Landing Pages !!
+        </div>
+        <BentoGrid className="w-full mx-auto  p-4 rounded-xl">
+          {items.map((item, i) => (
+            <BentoGridItem
+              key={i}
+              title={item.title}
+              description={item.description}
+              header={item.header}
+              icon={item.icon}
+              className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+              url={item.url}
+              color={item.color}
+            />
+          ))}
+        </BentoGrid>
+      </div>
     </div>
   );
 };
 
 export default Home;
 
-const Skeleton = ({img}:{img:string}) => (
+const Skeleton = ({ img }: { img: string }) => (
   <div className=" overflow-hidden  flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
-    <img className='bg-cover' src={img} alt="" />
+    <img className="w-full bg-cover" src={img} alt="" />
   </div>
 );
 const items = [
   {
-    url:"project1",
+    color: "pink-400",
+    url: "project1",
     title: "Bonus Gems",
-    description: "Landing Page",
+    // description: "Landing Page",
     header: <Skeleton img={img1} />,
-    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500  " />,
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-300  " />,
   },
   {
-    url:"project2",
+    color: "blue-400",
+    url: "project2",
     title: "Migliore Prestito",
     description: "",
     header: <Skeleton img={img2} />,
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-300" />,
   },
   {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton img="" />,
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+    color: "green-400",
+    url: "https://steady-frangollo-64fda9.netlify.app/",
+    title: "Balenciaga",
+    // description: "Join the quest for understanding and enlightenment.",
+    header: <Skeleton img={img3} />,
+    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-300" />,
   },
   {
-    title: "The Power of Communication",
-    description:
-      "Understand the impact of effective communication in our lives.",
-    header: <Skeleton img="" />,
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+    color: "violet-400",
+    url: "https://rainbow-bombolone-7a80ba.netlify.app/",
+    title: "Apple 2023",
+    // description: "Discover the beauty of thoughtful and functional design.",
+    header: <Skeleton img={img4} />,
+    icon: <IconSignature className="h-4 w-4 text-neutral-300" />,
   },
-  {
-    title: "The Pursuit of Knowledge",
-    description: "Join the quest for understanding and enlightenment.",
-    header: <Skeleton img="" />,
-    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Joy of Creation",
-    description: "Experience the thrill of bringing ideas to life.",
-    header: <Skeleton img="" />,
-    icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Spirit of Adventure",
-    description: "Embark on exciting journeys and thrilling discoveries.",
-    header: <Skeleton img="" />,
-    icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
-  },
+  // {
+  //   title: "The Power",
+  //   description:
+  //     "Understand the impact of effective communication in our lives.",
+  //   header: <Skeleton img="" />,
+  //   icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+  // },
+
+  // {
+  //   title: "The Joy of Creation",
+  //   description: "Experience the thrill of bringing ideas to life.",
+  //   header: <Skeleton img="" />,
+  //   icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+  // },
+  // {
+  //   title: "The Spirit of Adventure",
+  //   description: "Embark on exciting journeys and thrilling discoveries.",
+  //   header: <Skeleton img="" />,
+  //   icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+  // },
 ];
